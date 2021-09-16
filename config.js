f9d6b8659cb349dbfab5d3f6f4e74643cbc6f910
onkar41560 <72089245+onkar41560@users.noreply.github.com>
2021-09-16 20:34:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,8 +17,6 @@ SystemJS.config({
   map: {
     'plugin-babel': 'systemjs-plugin-babel@latest/plugin-babel.js',
     'systemjs-babel-build': 'systemjs-plugin-babel@latest/systemjs-babel-browser.js',
-    'react': 'react@17.0.1/umd/react.development.js',
-    'react-dom': 'react-dom@17.0.1/umd/react-dom.development.js'
   },
   transpiler: 'plugin-babel'
 });
