1fa0a81392cb9ce1ee7ae2c5202233ae5532c974
onkar41560 <72089245+onkar41560@users.noreply.github.com>
2021-09-16 12:43:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+import G from './g.js';
+ReactDOM.render(
+    <div>
+    <G/>
+    <G/>
+    <G/>
+    <G/>
+    <G/>
+    <G/>
+    <G/>
+    <G/>
+    </div>,
+    document.getElementById('react-app')
+    )
---
+++ g.js
@@ -0,0 +1,6 @@
+function G() {
+    return(
+        <h1>Sanjana</h1>
+        )
+}
+export default G;
